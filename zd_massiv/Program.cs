c327a4301313113328a5725c9942bf4531da0c0b
[YanaB] <[tudabae@gmail.com]>
2023-07-05 17:26:54 +0300

    massive

---
+++ zd_massiv/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int[] arr = getMassiv(8);
+
+int[] getMassiv(int size){
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++){
+        array[i] = new Random().Next(1, 29);
+    }return array;
+}
+Console.WriteLine($"{String.Join(", ", arr)}");
