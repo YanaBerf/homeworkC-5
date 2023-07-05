24d00598c3aa868fef72e3ccea778a491ecaa463
[YanaB] <[tudabae@gmail.com]>
2023-07-05 13:02:42 +0300

    возведение в степень

---
+++ zd_pow/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("enter number: ");
+double num = double.Parse(Console.ReadLine());
+Console.Write("enter number pow: ");
+double stepen = double.Parse(Console.ReadLine());
+
+double GetPow(double n, double s)
+{
+   double numPow = Math.Pow(n, s);
+   return numPow;
+}
+
+Console.WriteLine($"число {num} в степени {stepen} равно {GetPow(num, stepen)}");
