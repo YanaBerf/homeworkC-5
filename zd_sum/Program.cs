4e92dc0cf09f036e801dc557640a204df1e76ab1
[YanaB] <[tudabae@gmail.com]>
2023-07-05 17:26:22 +0300

    сумма чисел

---
+++ zd_sum/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetNumSum(int n)
+{   int sum = 0;
+    while(n > 0)
+    {
+        int  ostatok = n % 10;
+        n = n / 10;
+        sum = sum + ostatok;
+    }
+    return sum;  
+}
+Console.WriteLine(GetNumSum(num));
